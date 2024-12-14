3a3987f4e83967bf9f7c25d6877ef081a6973f4c
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-14 07:49:04 +0300

    Add class Turtle

---
+++ src/src/main/java/org/example/modul/Turtle.java
@@ -0,0 +1,41 @@
+package org.example.modul;
+
+public class Turtle extends Entity {
+
+    public Turtle() {}
+
+    public Turtle(int xcor, int ycor, int score) {
+        super("Черепаха", xcor, ycor, score);
+    }
+    @Override
+    public void move() {
+        int derection = (int) (Math.random() * 4);
+        switch (derection) {
+            case 0:
+                if (ycor > 0) {
+                    --ycor;
+                }
+                break;
+            case 1:
+                if (xcor < fwidth) {
+                    ++xcor;
+                }
+                break;
+            case 2:
+                if (ycor < fheigth) {
+                    ++ycor;
+                }
+                break;
+            case 3:
+                if (xcor > 0) {
+                    --xcor;
+                }
+                break;
+
+
+        }
+        ++score;
+    }
+
+}
+
