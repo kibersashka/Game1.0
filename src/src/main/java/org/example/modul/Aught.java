fcf7b9582f6e8e5caf475a11a427321360415018
kibersashka <aleksandraveldaeva4@gmail.com>
2024-12-14 07:51:34 +0300

    Add class Aught

--- src/src/main/java/org/example/modul/Aught.java
+++ src/src/main/java/org/example/modul/Aught.java
@@ -1,4 +1,19 @@
 package org.example.modul;
 
-public class Aught {
+public class Aught extends Entity {
+
+    public Aught() {
+    }
+
+    public Aught(int xcor, int ycor, int score){
+        super("Нечто", xcor, ycor, score);
+    }
+
+
+    @Override
+    public void move() {
+        xcor = (int) (Math.random() * (101));
+        ycor = (int) (Math.random() * (101));
+
+    }
 }
